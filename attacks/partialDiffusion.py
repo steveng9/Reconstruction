47cd608b3c46c6011411b3192b86efc4998ce592
steveng9 <golobs@uw.edu>
2026-02-07 18:50:32 -0800

    fix bug

--- attacks/partialDiffusion.py
+++ attacks/partialDiffusion.py
@@ -14,7 +14,7 @@ def partial_tabddpm_reconstruction(cfg, synth, targets, qi, hidden_features):
     meta, domain = get_meta_data_for_diffusion(cfg)
     if cfg["attack_params"]["retrain"]:
         train_diffusion_for_reconstruction(cfg, meta, domain, synth, qi, hidden_features)
-    reconstruction = reconstruct_data(cfg, targets, qi, hidden_features)
+    reconstruction = reconstruct_data_categorical(cfg, targets, qi, hidden_features)
     return reconstruction, None, None
 
 
@@ -25,6 +25,6 @@ def repaint_reconstruction(cfg, synth, targets, qi, hidden_features):
     meta, domain = get_meta_data_for_diffusion(cfg)
     if cfg["attack_params"]["retrain"]:
         train_diffusion_for_reconstruction(cfg, meta, domain, synth, qi, hidden_features, reconstruct_method_RePaint=True)
-    reconstruction = reconstruct_data(cfg, targets, qi, hidden_features, reconstruct_method_RePaint=True)
+    reconstruction = reconstruct_data_categorical(cfg, targets, qi, hidden_features, reconstruct_method_RePaint=True)
     return reconstruction, None, None
 
